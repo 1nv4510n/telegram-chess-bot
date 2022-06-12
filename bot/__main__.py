dba06462876966aab93ba51a541dd9253789c9e2
1nv4510n <bogdan226470@gmail.com>
2022-06-12 16:50:35 +0400

    logging support

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,3 @@
-import logging
-
 from aiogram import Bot, Dispatcher
 from aiogram.dispatcher.fsm.storage.memory import MemoryStorage
 from aiogram.dispatcher.fsm.storage.redis import RedisStorage
@@ -11,23 +9,20 @@ from .config import config
 from .commands import set_commands
 
 from .db.base import Base
+from .db.requests import reset_users_table
 from .middlewares import DbSessionMiddleware
+from .utils import log
 
 from .handlers.users import default, search_game, chess
 
-
 async def main():
-    logging.basicConfig(
-        level=logging.WARNING,
-        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-    )
-    
     engine = create_async_engine(config.postgres_dsn, future=True, echo=False)
 
     db_pool = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
     
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    # FIRST LAUNCH
+    # async with engine.begin() as conn:
+    #     await conn.run_sync(Base.metadata.create_all)
     
     bot = Bot(token=config.bot_token, parse_mode="HTML")
     
@@ -48,6 +43,16 @@ async def main():
     await set_commands(bot)
     
     try:
+        log.info('BOT STARTED')
         await dp.start_polling(bot, polling_timeout=60)
+    except Exception as e:
+        log.error(f'BOT START ERROR: {e}')
     finally:
+        try:
+            await reset_users_table(db_pool())
+        except Exception as e:
+            log.error(f'Reset users table error: {e}')
+        else:
+            log.info('Reset users table DONE')
+        log.info('BOT STOPPED')
         await bot.session.close()
